e73e22f671d3fdcb0a2aebb9627a56b73f7d5ddd
NaranjoJimenezAndres <andresgassgass@gmail.com>
2024-08-12 11:32:13 +0200

    changes

---
+++ src/components/Player.jsx
@@ -0,0 +1,44 @@
+import { useState, useRef } from 'react';
+
+
+export default function Player() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [CurrentSong, setCurrentSong] = useState(null);
+    const audioRef = useRef();
+    const handleClick = () => {
+        debugger
+        if (isPlaying) {
+            audioRef.current.pause();
+        } else {
+            audioRef.current.src= `/music/1/01.mp3`
+            audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+        }
+    return (
+        <div className="flex flex-row justify-between w-full px-4 z-50">
+            <div>
+                CurrentSong...
+            </div>
+
+            <div className="grid place-content-center gap-4 flex-1">
+                <div className="flex justify-center">
+                    <button className="bg-white rounded-full p-2" onClick={handleClick}>
+                        {isPlaying ? 
+                            <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg> :
+                            <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
+                        }
+                    </button>
+                </div>
+            </div>
+
+            <div>
+                Volumen
+            </div>
+
+
+        </div>
+
+    )
+}
+
